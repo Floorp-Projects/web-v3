5e10856dc5ac3a99c7a7d7a8e5122aff840ac1ee
Ryosuke Asano <s1f102400011@iniad.org>
2025-04-27 00:38:47 +0900

    feat(i18n): import i18n configuration in the main app file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app";
 import "./globals.css";
 import Layout from "../components/Layout";
 import { appWithTranslation } from "next-i18next";
+import "../i18n";
 
 function App({ Component, pageProps }: AppProps) {
     return (
